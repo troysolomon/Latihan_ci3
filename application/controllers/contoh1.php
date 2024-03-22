d4e76ceff650d24415462612e6e882bc4027cf79
troysolomon <troysolomonpantouw564@gmail.com>
2024-03-22 17:04:10 +0700

    latihan 1 CI3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller 
+
+    {
+        public function index()
+        {
+            echo "<h1>Perkenalkan nama saya adalah...<h1>";
+            echo "<h3>Troy Solomon Pantouw<h3>";
+        }
+    }
+?>
---
+++ application/views/view_latihan1.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Latihan 1</title>
+    </head>
+    <body>
+        Halo Kawan Kawan.. mari kita belajar programming!!<br>
+        Nilai 1 = <?= $nilai1;?>
+        Nilai 2 = <?= $nilai2;?>
+        Ini adalah hasil pemodelan dengan metode penjumlahan yaitu <?=$nilai1."+".$nilai2."=".$hasil;?>
+
+    </body>
+</html>
